6145db667a3f5ce5dfc980bfedb5892f3cc848e6
SebastianMorado <sabby.morado@gmail.com>
2021-12-29 17:37:28 +0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Sebastian Morado on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
